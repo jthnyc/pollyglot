64119e8f6fd58e2bc42f9613b23b3dcdd477b976
Joanna Huang <joannathhuang@gmail.com>
2024-10-30 15:21:38 -0400

    Minor clean up

--- completions.mjs
+++ completions.mjs
@@ -1,9 +1,9 @@
-import OpenAI from "openai";
+import OpenAI from 'openai';
 import 'dotenv/config';
 import express from 'express';
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 import path from 'path';
-import { fileURLToPath } from "url";
+import { fileURLToPath } from 'url';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -11,9 +11,9 @@ const __dirname = path.dirname(__filename);
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'api')));
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, () => console.log('Server running on port 3000'));
 
-app.post("/gpt", async (req, res) => {
+app.post('/gpt', async (req, res) => {
     let prompt = req.body.prompt;
     let language = req.body.language;
     let response = await fetchTextCompletion(prompt, language);
@@ -27,11 +27,11 @@ const openai = new OpenAI({
 
 export async function fetchTextCompletion(prompt, language) {
     const completion = await openai.chat.completions.create({
-        model: "gpt-4o-mini",
+        model: 'gpt-4o-mini',
         messages: [
-            { role: "system", content: "You are a translator who only provides the translation." },
+            { role: 'system', content: 'You are a translator who only provides the translation.' },
             {
-                role: "user",
+                role: 'user',
                 content: `Translate ${prompt} to ${language}.`,
             },
         ],
