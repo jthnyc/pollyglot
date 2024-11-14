b8211aa3240a61984d28faafd87aa55d3dc2c103
Joanna Huang <joannathhuang@gmail.com>
2024-11-14 16:01:23 -0500

    [Feat]: add tone to options

--- public/index.js
+++ public/index.js
@@ -5,10 +5,12 @@ const startOverText = 'Start Over';
 const inputTitle = document.querySelector('.translation-input__title');
 const textInputArea = document.querySelector('.translation-input__textarea');
 const translationTextArea = document.querySelector('.translation__textarea');
+const toneOptions = document.querySelectorAll('.tone-select__option');
 const languageOptions = document.querySelectorAll('.language-select__option');
 const translationSection = document.querySelector('.translation');
 const translateCTA = document.querySelector('.translate-cta');
 
+let selectedTone = 'formal'
 let selectedLanguage = availableLanguages[0];
 let textToTranslate = '';
 
@@ -18,33 +20,53 @@ function setEventListeners() {
         toggleTitleAndCTAState();
     })
 
+    toneOptions.forEach(option => option.addEventListener('click', (e) => updateSelectedTone(e)));
+
     languageOptions.forEach(option => option.addEventListener('click', (e) => updateSelectedLanguage(e)));
 
     translateCTA.addEventListener('click', handleCTA);
 }
 
+function updateSelectedTone(e) {
+    const selectedOption = findClosestOption(e, 'tone');
+    selectedTone = selectedOption;
+    updateRadioSelection(selectedOption, 'tone');
+}
+
 function updateSelectedLanguage(e) {
-    const selectedOption = findClosestOption(e);
+    const selectedOption = findClosestOption(e, 'language');
     const languageIndex = availableLanguages.indexOf(selectedOption);
     selectedLanguage = availableLanguages[languageIndex];
-    updateRadioSelection(selectedLanguage);
+    updateRadioSelection(selectedLanguage, 'language');
+    if (selectedLanguage === 'Chinese') {
+        selectedLanguage = 'Traditional Chinese'
+    }
 }
 
-function findClosestOption(e) {
-    const closestOption = e.target.closest('.language-select__option');
-    const selectedLang = closestOption && closestOption.querySelector('input').value;
-    return selectedLang;
+function findClosestOption(e, type) {
+    const closestOption = type === 'language' ? e.target.closest('.language-select__option') : e.target.closest('.tone-select__option');
+    const selectedOption = closestOption && closestOption.querySelector('input').value;
+    return selectedOption;
 }
 
-function updateRadioSelection(selectedLanguage) {
-    console.log('selectedLanguage: ', selectedLanguage)
-    languageOptions.forEach(option => {
-        const radioInput = option.querySelector('input');
-        radioInput.removeAttribute('checked');
-        if (radioInput.id === selectedLanguage) {
-            radioInput.checked = true;
-        }
-    })
+function updateRadioSelection(selectedLanguage, type) {
+    if (type === 'language') {
+        languageOptions.forEach(option => {
+            const radioInput = option.querySelector('input');
+            radioInput.removeAttribute('checked');
+            if (radioInput.id === selectedLanguage) {
+                radioInput.checked = true;
+            }
+        })
+    } else {
+        toneOptions.forEach(option => {
+            const radioInput = option.querySelector('input');
+            radioInput.removeAttribute('checked');
+            if (radioInput.id === selectedTone) {
+                radioInput.checked = true;
+            }
+        })
+    }
 }
 
 function toggleTitleAndCTAState() {
@@ -70,10 +92,13 @@ function toggleReadOnly() {
 
 function toggleUIDisplay() {
     const secondaryInputTitle = document.querySelector('.translation-input__title-secondary');
+    const toneSelectSection = document.querySelector('.tone-select');
     const languageSelectSection = document.querySelector('.language-select');
+    const translationTitle = document.querySelector('.translation__title');
     if (languageSelectSection.classList.contains('hidden')) {
         inputTitle.classList.remove('hidden');
         secondaryInputTitle.classList.add('hidden');
+        toneSelectSection.classList.remove('hidden');
         languageSelectSection.classList.remove('hidden');
         translationSection.classList.add('hidden');
         textToTranslate = '';
@@ -83,9 +108,11 @@ function toggleUIDisplay() {
     } else {
         inputTitle.classList.add('hidden');
         secondaryInputTitle.classList.remove('hidden');
+        toneSelectSection.classList.add('hidden');
         languageSelectSection.classList.add('hidden');
         translationSection.classList.remove('hidden');
         translateCTA.innerText = startOverText;
+        translationTitle.innerText = `${selectedLanguage} Polly:`;
     }
 }
 
@@ -110,6 +137,7 @@ async function fetchTranslation() {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
+                'tone': selectedTone,
                 'prompt': textToTranslate,
                 'language': selectedLanguage
             })
